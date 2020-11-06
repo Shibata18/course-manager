1a1e97ce6301b148abdb968fe579e18b83974e10
Douglas Shibata <douglas.shibata11@gmail.com>
2020-11-06 15:25:24 -0300

    Atualizando diretórios e tratando erros

--- src/app/courses/course-info.component.ts
+++ src/app/courses/course-info.component.ts
@@ -4,16 +4,26 @@ import { Course } from './course';
 import { CourseService } from './course.service';
 
 @Component({
-    templateUrl:'./course-info.component.html'
+    templateUrl: './course-info.component.html'
 })
-export class CourseInfoComponent implements OnInit{
-    course:Course;
+export class CourseInfoComponent implements OnInit {
 
-    constructor(private activatedRoute:ActivatedRoute,private couseService:CourseService){}
-    ngOnInit():void{
-        this.course =this.couseService.retriveById( +this.activatedRoute.snapshot.paramMap.get('id'));
+    course: Course;
+
+    constructor(private activatedRoute: ActivatedRoute, private courseService: CourseService) { }
+    
+    ngOnInit(): void { 
+        this.courseService.retrieveById(+this.activatedRoute.snapshot.paramMap.get('id')).subscribe({
+            next: course => this.course = course,
+            error: err => console.log('Error', err)
+        });
     }
-    save():void{
-        this.couseService.save(this.course)
+
+    save(): void {
+        this.courseService.save(this.course).subscribe({
+            next: course => console.log('Saved with success', course),
+            error: err => console.log('Error', err)
+        });
     }
+
 }
--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,34 +1,52 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 import { Course } from './course';
 import { CourseService } from './course.service';
 
 @Component({
     templateUrl: './course-list.component.html'
 })
-export class CourseListComponent implements OnInit {
+export class CourseListComponent implements OnInit { 
+
     filteredCourses: Course[] = [];
+
     _courses: Course[] = [];
+    
     _filterBy: string;
+
     constructor(private courseService: CourseService) { }
-    ngOnInit(): void {
-        this.retriveAll()
+
+    ngOnInit(): void { 
+        this.retrieveAll();
     }
-    retriveAll():void{
-        this.courseService.retriveAll().subscribe({
-            next:courses=>{
-                this._courses = courses
+
+    retrieveAll(): void { 
+        this.courseService.retrieveAll().subscribe({
+            next: courses => {
+                this._courses = courses;
                 this.filteredCourses = this._courses;
             },
-            error:err=>console.log("Error: ",err)
-            
+            error: err => console.log('Error', err) 
         })
-
     }
-    set filter(value: string) {
+
+    deleteById(courseId: number): void { 
+        this.courseService.deleteById(courseId).subscribe({
+            next: () => { 
+                console.log('Deleted with success');
+                this.retrieveAll();
+            },
+            error: err => console.log('Error', err)
+        })
+    }
+
+    set filter(value: string) { 
         this._filterBy = value;
+
         this.filteredCourses = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1);
     }
-    get filter() {
-        return this._filterBy
+
+    get filter() { 
+        return this._filterBy;
     }
+
 }
